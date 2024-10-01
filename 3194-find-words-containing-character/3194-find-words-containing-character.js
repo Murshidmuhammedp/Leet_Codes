9c4d1952b4768429f6e2a388efe8caf7d9624259
Murshid muhammed p <159038011+Murshidmuhammedp@users.noreply.github.com>
2024-10-01 11:00:39 +0530

    Time: 77 ms (47.43%), Space: 53.4 MB (21.92%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function (words, x) {
+    const arr = []
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words[i].length; j++) {
+            if (words[i][j] == x) {
+                arr.push(i)
+            }
+        }
+    }
+    return arr1 = [...new Set(arr)]
+};
