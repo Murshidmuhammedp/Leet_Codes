029d966f9ca0cd013c32b42d2dde95094a4fd679
Murshid muhammed p <159038011+Murshidmuhammedp@users.noreply.github.com>
2024-10-04 16:37:49 +0530

    Time: 57 ms (32.9%), Space: 48.9 MB (35.12%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.js
@@ -5,8 +5,8 @@
 var numberOfSteps = function (num) {
     var count = 0
     while (num > 0) {
-        num % 2 == 0 ? num = num / 2 : num = num - 1;
-        count ++
+        num % 2 == 0 ? num = num / 2 : num--;
+        count++
     }
     return count
 };
