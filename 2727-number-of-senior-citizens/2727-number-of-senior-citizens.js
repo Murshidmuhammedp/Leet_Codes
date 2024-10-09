d45c1d59c8c7d9ba52932c8df78af1e31aeb54c9
Murshid muhammed p <159038011+Murshidmuhammedp@users.noreply.github.com>
2024-10-09 09:44:05 +0530

    Time: 65 ms (17.97%), Space: 50.3 MB (88.04%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.js
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.js
@@ -5,10 +5,7 @@
 var countSeniors = function (details) {
     var count = 0
     for (let i = 0; i < details.length; i++) {
-        const age = details[i].slice(11, 13)
-        if (age > 60) {
-            count++
-        }
+        if (details[i].slice(11, 13) > 60) count++
     }
     return count
 };
