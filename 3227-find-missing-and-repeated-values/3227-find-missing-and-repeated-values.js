8e53b3a8331181a8e8adee0c8f70c7e2b498be73
Murshid muhammed p <159038011+Murshidmuhammedp@users.noreply.github.com>
2024-10-10 15:05:14 +0530

    Time: 247 ms (5.06%), Space: 55.5 MB (20.22%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.js
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.js
@@ -4,10 +4,8 @@
  */
 var findMissingAndRepeatedValues = function (grid) {
     const arr1 = []
-    const arr = grid.flat().sort((a,b)=>a-b)
-    const a = arr.filter((val, _, ar) => {
-        return ar.indexOf(val) !== ar.lastIndexOf(val)
-    })
+    const arr = grid.flat().sort((a, b) => a - b)
+    const a = arr.filter((val, _, ar) => ar.indexOf(val) !== ar.lastIndexOf(val))
     arr1.push(a[0])
     const newarr = [...new Set(arr)]
     for (let i = 0; i <= newarr.length; i++) {
