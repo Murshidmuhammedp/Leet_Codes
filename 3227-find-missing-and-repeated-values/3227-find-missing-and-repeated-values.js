90a94a8249d2c422495a11d6b1ccc4cf507359ea
Murshid muhammed p <159038011+Murshidmuhammedp@users.noreply.github.com>
2024-10-10 15:02:54 +0530

    Time: 243 ms (5.06%), Space: 55.2 MB (25.27%) - LeetHub

---
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[]}
+ */
+var findMissingAndRepeatedValues = function (grid) {
+    const arr1 = []
+    const arr = grid.flat().sort((a,b)=>a-b)
+    const a = arr.filter((val, _, ar) => {
+        return ar.indexOf(val) !== ar.lastIndexOf(val)
+    })
+    arr1.push(a[0])
+    const newarr = [...new Set(arr)]
+    for (let i = 0; i <= newarr.length; i++) {
+        if (newarr[i] !== i + 1) {
+            arr1.push(i + 1)
+            break;
+        }
+    }
+    return arr1
+};
