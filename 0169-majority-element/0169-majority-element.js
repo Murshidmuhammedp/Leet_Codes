7be0657ba849c2b7ae4a3133fad9c5e3b0c7e801
Murshid muhammed p <159038011+Murshidmuhammedp@users.noreply.github.com>
2024-10-15 10:27:40 +0530

    Time: 66 ms (32.94%), Space: 53.8 MB (11.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,18 +3,13 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-        if(nums.length<=1)return nums[0]
+    if (nums.length <= 1) return nums[0]
     const a = nums.sort().length / 2
-    // console.log(nums,a)
     var count = 1
     for (let i = 0; i < nums.length; i++) {
-        // console.log(count)
         if (nums[i] == nums[i + 1]) {
             count += 1
-            if (count > a) {
-                //   console.log("hii",nums[i])
-                return nums[i]
-            }
+            if (count > a) return nums[i]
         }
     }
     count = 1
