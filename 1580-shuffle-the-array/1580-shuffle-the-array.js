6e4c9354e8b75a049174658c9f7d466ee0548606
Murshid muhammed p <159038011+Murshidmuhammedp@users.noreply.github.com>
2024-12-04 15:21:14 +0530

    Time: 60 ms (64.84%), Space: 52.5 MB (21.23%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.js
+++ 1580-shuffle-the-array/1580-shuffle-the-array.js
@@ -5,10 +5,8 @@
  */
 var shuffle = function (nums, n) {
     var newArray = []
-    var a = nums.slice(0, n);
-    var b = nums.slice(n);
     for (let i = 0; i < n; i++) {
-        newArray.push(a[i], b[i])
+        newArray.push(nums[i],nums[i+n])
     }
     return newArray
 };
