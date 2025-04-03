10c868842f0943e3a38116a0dd1ec5dfc399ac11
Murshid Muhammed P <159038011+Murshidmuhammedp@users.noreply.github.com>
2025-04-03 12:24:22 +0530

    Time: 2 ms (84.52%), Space: 54.3 MB (59.41%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.js
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.js
@@ -7,9 +7,7 @@ var countPairs = function (nums, k) {
     var count = 0
     for (i = 0; i < nums.length; i++) {
         for (j = i + 1; j < nums.length; j++) {
-            if (nums[i] == nums[j]) {
-                if ((i * j) % k == 0) count++
-            }
+            if (nums[i] == nums[j] && (i * j) % k == 0) count++
         }
     }
     return count
