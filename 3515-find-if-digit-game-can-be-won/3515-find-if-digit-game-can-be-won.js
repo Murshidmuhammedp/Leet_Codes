9e0d3228b687b503b534bf9dec89b6a00bb3ade6
Murshid Muhammed P <159038011+Murshidmuhammedp@users.noreply.github.com>
2025-04-08 15:22:28 +0530

    Time: 0 ms (100%), Space: 56 MB (41.69%) - LeetHub

--- 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.js
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.js
@@ -2,15 +2,11 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canAliceWin = function(nums) {
-    var num=0
-    var num1=0
-    for(i=0;i<nums.length;i++){
-       if(nums[i]<10){
-            num += nums[i]
-       }else{
-            num1 +=nums[i]
-       }
+var canAliceWin = function (nums) {
+    var num = 0
+    var num1 = 0
+    for (i = 0; i < nums.length; i++) {
+        nums[i] < 10 ? num += nums[i] : num1 += nums[i];
     }
-    return num!==num1
+    return num !== num1
 };
